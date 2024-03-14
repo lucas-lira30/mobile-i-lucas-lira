b413aa3f5c441a2a4d2fb2354b4d941df3c5b1e2
lucas-lira30 <160851018+lucas-lira30@users.noreply.github.com>
2024-03-14 08:20:08 -0300

    Add files via upload

---
+++ aula03/funcoes1.js
@@ -0,0 +1,16 @@
+function Soma ( ){
+    let n1= 2;
+    let n2= 6;
+console.log (n1+n2);
+
+}
+Soma ()
+Soma ()
+Soma ()
+
+function SomaComParamentros (n1, n2) {
+    console.log (n1 + n2)
+}
+
+SomaComParamentros (8, 10)
+SomaComParamentros (50, 10)
